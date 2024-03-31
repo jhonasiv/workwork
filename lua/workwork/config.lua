0cf6afa26afdad15fe1155510649cdeac98b4751
Jhonas Moura <jhonaspradomoura@gmail.com>
2024-03-30 22:46:28 -0300

    fix: add support for relative path in telescope files

--- lua/workwork/config.lua
+++ lua/workwork/config.lua
@@ -7,8 +7,11 @@ M.default = {
 	integrations = {
 		telescope = {
 			enable = false,
-			find_opts = { "-type", "f", "-not", "-path", "'*/\\.git/*'", "-printf", "'%P\n'" },
-			fd_opts = { "--color=never", "--type", "f", "--hidden", "--follow", "--exclude", ".git" },
+			opts = {
+				find_opts = { "-type", "f", "-not", "-path", "'*/\\.git/*'", "-printf", "'%P\n'" },
+				fd_opts = { "--color=never", "--type", "f", "--hidden", "--follow", "--exclude", ".git" },
+				relative_path_entries = false,
+			},
 		},
 		fzf_lua = {
 			enable = false,
